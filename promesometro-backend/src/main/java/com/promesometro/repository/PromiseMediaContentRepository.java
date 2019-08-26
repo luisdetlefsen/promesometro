90d4e9aef7ee07c9c30b71971bd9af8726e536b2
Luis Detlefsen <luis.detlefsen@taskeasy.com>
2019-08-26 01:02:12 -0600

    Retrieve promise media by promise id.

--- promesometro-backend/src/main/java/com/promesometro/repository/PromiseMediaContentRepository.java
+++ promesometro-backend/src/main/java/com/promesometro/repository/PromiseMediaContentRepository.java
@@ -1,9 +1,16 @@
 package com.promesometro.repository;
 
 import com.promesometro.entities.PromiseMediaContent;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "promiseMedia", path="promiseMediaContent")
 public interface PromiseMediaContentRepository extends PagingAndSortingRepository<PromiseMediaContent, Long> {
+
+    @Query("select pmc from PromiseMediaContent pmc where pmc.promise.idPromise = :idPromise")
+    List<PromiseMediaContent> findAllByPromise(@Param("idPromise") Long idPromise);
 }
